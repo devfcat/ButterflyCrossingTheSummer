64a98b8ebe446adac9ae2322dc3f3c93f90cb712
devfcat <devfcat@gmail.com>
2025-07-29 02:21:15 +0900

    Update Panel_Main.cs

--- Assets/Scripts/Main/Panel_Main.cs
+++ Assets/Scripts/Main/Panel_Main.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class Panel_Main : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public void OnClick_Setting()
     {
-        
+        GameManager.Instance.Control_Setting();
     }
 }
