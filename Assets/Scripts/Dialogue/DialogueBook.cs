496d1c55dd85525b143b2eb2e6264951669b3c4a
devfcat <devfcat@gmail.com>
2025-07-29 03:09:58 +0900

    [feat] 대사 정보 구조 설계

---
+++ Assets/Scripts/Dialogue/DialogueBook.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 챕터의 스크립트와 세부 셀의 데이터구조
+/// </summary>
+public class DialogueBook
+{
+    public List<DialogueCell> dialogues;
+}
+
+/// <summary>
+/// 한 대사 데이터에 담겨져있는 정보
+/// </summary>
+public class DialogueCell
+{
+    public string? speaker;
+    public string content;
+    public string? scg;
+    public string bg;
+    public string? ecg;
+    public string bgm;
+    public string? se;
+    public bool fade;
+    public bool isChoice;
+    public string? choiceResult;
+    public string? choiceScore;
+}
