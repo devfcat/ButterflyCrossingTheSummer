33ea32e5e43a0931a2aaa57018c26ae05b721830
devfcat <devfcat@gmail.com>
2025-07-30 02:10:23 +0900

    [feat] 메인 메뉴 갤러리, 새로하기, 나가기 기능 작업

--- Assets/Scripts/Main/Panel_Main.cs
+++ Assets/Scripts/Main/Panel_Main.cs
@@ -4,9 +4,28 @@ using UnityEngine;
 
 public class Panel_Main : MonoBehaviour
 {
+    [Header("메인 화면의 패널들")]
+    public GameObject Panel_Gallery;
 
     public void OnClick_Setting()
     {
         GameManager.Instance.Control_Setting();
+    }
+
+    public void OnClick_Gallery()
+    {
+        GameManager.Instance.Control_Popup(true, Panel_Gallery);
+    }
+
+    public void OnClick_New()
+    {
+        SoundManager.Instance.PlaySFX(SFX.UI);
+        GameManager.Instance.SetState(eState.Chapter1);
+    }
+
+    public void OnClick_Quit()
+    {
+        Application.Quit();
+        Debug.Log("게임 종료");
     }
 }
