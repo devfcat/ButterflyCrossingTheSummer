d5632c4b36bd7f6ec24d2d82a86e34abdd35cce8
devfcat <devfcat@gmail.com>
2025-08-06 08:08:16 +0900

    [design] ESG가 처음 등장할 때 1초간 애니메이션

---
+++ Assets/Scripts/Images/ECG_Animation.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ECG_Animation : MonoBehaviour
+{
+    public void Close()
+    {
+        this.gameObject.SetActive(false);
+    }
+}
