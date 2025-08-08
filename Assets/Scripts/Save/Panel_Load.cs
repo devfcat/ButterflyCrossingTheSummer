03a27940b1102108800d76a4520aa0ba59641441
Devfcat <137423690+devfcat@users.noreply.github.com>
2025-08-09 06:33:08 +0900

    [feat] 세이브가 없을 시의 UI 처리

--- Assets/Scripts/Save/Panel_Load.cs
+++ Assets/Scripts/Save/Panel_Load.cs
@@ -4,10 +4,24 @@ using UnityEngine;
 
 public class Panel_Load : MonoBehaviour
 {
+    public GameObject label_notexist;
+    public GameObject savePage;
+
     void OnEnable()
     {
         SaveManager.Instance.Load_Data();
         SaveManager.Instance.Set_UI();
+
+        if (SaveManager.Instance.saveDatas.Count == 0)
+        {
+            label_notexist.SetActive(true);
+            savePage.SetActive(false);
+        }
+        else
+        {
+            label_notexist.SetActive(false);
+            savePage.SetActive(true);
+        }
     }
 
     public void Close()
