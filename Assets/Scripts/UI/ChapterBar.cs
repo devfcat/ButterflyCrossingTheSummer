964a163d8e90d64c1f4951628d88729b30e4cb95
Devfcat <137423690+devfcat@users.noreply.github.com>
2025-08-12 01:50:41 +0900

    [feat] 씬 시작 시 몇 챕터인지 알려주는 UI 작업

---
+++ Assets/Scripts/UI/ChapterBar.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ChapterBar : MonoBehaviour
+{
+    public TextMeshProUGUI tmp_chapter;
+
+    void OnEnable()
+    {
+        SetChapterUI();
+    }
+
+    void SetChapterUI()
+    {
+        tmp_chapter.text = GameManager.Instance.m_State.ToString();
+    }
+}
