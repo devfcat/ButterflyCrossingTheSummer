02198526bc511554faa03853da4dc37b4853e8ef
Devfcat <137423690+devfcat@users.noreply.github.com>
2025-08-27 21:30:53 +0900

    [feat] 시작 시 풀 소리가 나도록 수정

--- Assets/Scripts/Endings/Credits.cs
+++ Assets/Scripts/Endings/Credits.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 
 public class Credits : MonoBehaviour
 {
-    void OnEnable()
+    public void OnEnable()
+    {
+        SoundManager.Instance.PlayBGM(BGM.BGM_Grasses);
+    }
+    
+    public void StartBGM()
     {
         SoundManager.Instance.Init();
         SoundManager.Instance.PlayBGM(BGM.BGM_RealEnd);
